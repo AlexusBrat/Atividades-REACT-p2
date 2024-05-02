cc6a2e331187f3fd71743868b558f40e67f06a34
AlexusBrat <alexxbrati@gmail.com>
2024-05-02 17:39:48 -0300

    alteração

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Relogio from './functions/Relogio';
 import Letreiro from './functions/Letreiro';
-setInterval(() => {Relogio()});
 
 export default function App() {
 
   return (
     <div className="App">
-        <h3 id="relogio">00:00:00</h3>
+        <Relogio />
         <Letreiro />
     </div>
   );
--- src/functions/Relogio.js
+++ src/functions/Relogio.js
@@ -1,17 +1,16 @@
 export default function Relogio () {
-    let data = new Date()
-    let hora = data.getHours()
-    let minuto = data.getMinutes()
-    let segundo = data.getSeconds()
-    
-    // Operadores Ternários para concatenar números a baixo de 10. Ex: 01, 02, 03 ... 09.
-    // condição ? true : false
-    hora < 10 ? hora = `0${data.getHours()}` : hora = data.getHours()
-    minuto < 10 ? minuto = `0${data.getMinutes()}` : minuto = data.getMinutes() 
-    segundo < 10 ? segundo = `0${data.getSeconds()}` : segundo = data.getSeconds()
-    
-    document.getElementById("relogio").innerHTML = `${hora}:${minuto}:${segundo}`
 
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    let segundo = data.getSeconds();
     
+    hora < 10 ? hora = `0${data.getHours()}` : hora = data.getHours();
+    minuto < 10 ? minuto = `0${data.getMinutes()}` : minuto = data.getMinutes();
+    segundo < 10 ? segundo = `0${data.getSeconds()}` : segundo = data.getSeconds();
+    setInterval(() => {});
 
+    return (
+        <h3 id="relogio">{ hora }:{ minuto }: {segundo} </h3>
+    )
 }
